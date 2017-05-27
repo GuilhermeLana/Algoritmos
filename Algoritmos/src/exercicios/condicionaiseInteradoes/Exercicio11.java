b9ae02a763b40daabc25370f44abc3c88e279a44
Noturno <Noturno@192.168.7.180>
2017-05-26 21:52:34 -0300

    26/05

--- Algoritmos/src/exercicios/condicionaiseInteradoes/Exercicio11.java
+++ Algoritmos/src/exercicios/condicionaiseInteradoes/Exercicio11.java
@@ -1,7 +1,29 @@
 package exercicios.condicionaiseInteradoes;
 
+import java.util.Scanner;
+
 public class Exercicio11 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num1;
+		System.out.println("Digite um número:");
+		num1 = scanner.nextInt();
+		int num2;
+		System.out.println("Digite outro número:");
+		num2 = scanner.nextInt();
+		int num3;
+		System.out.println("Digite outro número:");
+		num3 = scanner.nextInt();
+		int maior;
+		if(num1 > num2 & num1 > num3){
+			maior = num1;
+		} else if(num2 > num3){
+				maior = num2;
+			} else {
+				maior = num3;
+			}
+		System.out.println("O maior número é " + maior);
+		scanner.close();
 		
 	}
 
