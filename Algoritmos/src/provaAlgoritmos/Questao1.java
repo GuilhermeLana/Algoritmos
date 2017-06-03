dabeaf58525336f6f61e02964fd1f8455ea4291d
Noturno <Noturno@Sala913>
2017-06-02 21:52:27 -0300

    prova

---
+++ Algoritmos/src/provaAlgoritmos/Questao1.java
@@ -0,0 +1,18 @@
+package provaAlgoritmos;
+
+public class Questao1 {
+	public static void main(String[] args) {
+		int numeros[] = new int[10];
+		int x = 10;
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = x;
+			x--;
+			if(numeros[i] % 2 == 0){
+				System.out.println(numeros[i] + " - Par");
+			} else {
+				System.out.println(numeros[i] + " - Impar");
+			}
+		}
+	}
+
+}
